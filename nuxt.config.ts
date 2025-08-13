4c603bfa99cb570bb1816133848e9f1adeb16ff5
Rufree <tksk2884@gmail.com>
2025-08-14 01:14:21 +0900

    nuxt.config theme.css 설정 적용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,23 @@ export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
 
+    devServer: {
+        host: "0.0.0.0",
+        port: 3000,
+    },
+
+    app: {
+        head: {
+            script: [
+                {
+                    src: `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.VUE_APP_KAKAO_JAVASCRIPT_KEY}&autoload=false`,
+                    defer: true,
+                },
+            ],
+        },
+    },
+
     modules: ["@pinia/nuxt"],
-    css: ["~/assets/css/main.css"],
+
+    css: ["~/assets/css/main.css", "~/assets/css/theme.css"],
 });
