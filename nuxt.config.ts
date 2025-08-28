0a4db3b375dd85c2c292f11526dff47150fa7160
Rufree <tksk2884@gmail.com>
2025-08-29 01:05:22 +0900

    api base / app head 설정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,13 @@ export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
 
+    runtimeConfig: {
+        public: {
+            origin: process.env.VUE_APP_ORIGIN,
+            apiBase: process.env.VUE_APP_API_URL || "http://localhost:8567",
+        },
+    },
+
     devServer: {
         host: "0.0.0.0",
         port: 3000,
@@ -12,6 +19,8 @@ export default defineNuxtConfig({
 
     app: {
         head: {
+            title: "ArtRoad",
+            meta: [{ name: "description", content: "전시회 정보 아트로드" }],
             script: [
                 {
                     src: `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.VUE_APP_KAKAO_JAVASCRIPT_KEY}&autoload=false`,
